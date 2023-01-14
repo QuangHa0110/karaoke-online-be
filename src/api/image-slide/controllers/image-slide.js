7f85d2929b41c3bef07b25dfe15fe9cd181ec250
Hân Phạm Văn <hoanlazy123@gmail.com>
2023-01-14 07:43:27 +0000

    init image slide

---
+++ src/api/image-slide/controllers/image-slide.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * image-slide controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::image-slide.image-slide');
---
+++ src/api/image-slide/services/image-slide.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * image-slide service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::image-slide.image-slide');
